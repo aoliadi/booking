ea1856f4df649131be93b4eb424872b5f3f94b35
aoliadi <aoliadi1998@gmail.com>
2022-06-30 04:39:20 +0100

    navbar done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
 import nav from "./navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={nav.container}>
       <ul className={nav.list_container}>
-        <li className="">
-          <span className="logo logo--nav">lamabooking</span>
+        <li className={nav.fullwidth}>
+          <Link to="/" className="">
+            lamabooking
+          </Link>
+          <span className="logo logo--nav"></span>
         </li>
         <li className="">
-          <button className="">register</button>
+          <button className={nav.btn}>register</button>
         </li>
         <li className="">
-          <button className="">login</button>
+          <button className={nav.btn}>login</button>
         </li>
       </ul>
     </div>
