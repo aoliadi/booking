323d9063369399c4107686d7662594d16bac8c81
aoliadi <aoliadi1998@gmail.com>
2022-06-30 10:09:06 +0100

    done with header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Home from "./pages/home/Home";
 import Hotel from "./pages/hotels/Hotel";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/hotels" element={<Hotel />}></Route>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,225 @@
+import header from "./header.module.css";
+import search from "./search.module.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalendarDays,
+  faContactBook,
+} from "@fortawesome/free-regular-svg-icons";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { format } from "date-fns";
+
+function Header() {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const handleOptionBtn = (type, action) => {
+    switch (action) {
+      case "increment":
+        setOptions((prev) => {
+          return {
+            ...prev,
+            [type]: options[type] + 1,
+          };
+        });
+        break;
+      case "decrement":
+        setOptions((prev) => {
+          return {
+            ...prev,
+            [type]: options[type] - 1,
+          };
+        });
+        break;
+      default:
+        alert("error!");
+        break;
+    }
+  };
+
+  return (
+    <header className={header.container}>
+      <ul className={header.list__container}>
+        <li className={`${header.list__item} ${header.active}`}>
+          <FontAwesomeIcon icon={faBed} />
+          <span className="">Stays</span>
+        </li>
+        <li className={header.list__item}>
+          <FontAwesomeIcon icon={faPlane} />
+          <span className="">Flights</span>
+        </li>
+        <li className={header.list__item}>
+          <FontAwesomeIcon icon={faCar} />
+          <span className="">Car rentals</span>
+        </li>
+        <li className={header.list__item}>
+          <FontAwesomeIcon icon={faBed} />
+          <span className="">Attractions</span>
+        </li>
+        <li className={header.list__item}>
+          <FontAwesomeIcon icon={faTaxi} />
+          <span className="">Airport taxis</span>
+        </li>
+      </ul>
+      <article className={header.heading}>
+        <h1 className={header.title}>
+          A lifetime of discounts? It's a Genius.
+        </h1>
+        <p className={header.desc}>
+          Get rewarded for your travels - unlock instant savings of 10% or more
+          with a free Lamabooking account
+        </p>
+        <button className={header.btn}>sign in</button>
+      </article>
+      <section className={search.container}>
+        <ul className={search.list__container}>
+          <li className={search.item}>
+            <FontAwesomeIcon icon={faBed} className={search.icon} />
+            <input
+              type="text"
+              name="location"
+              id="location"
+              placeholder="Where are you going?"
+              className={search.input}
+            />
+          </li>
+          <li className={search.item}>
+            <FontAwesomeIcon
+              onClick={() => setShowCalendar(!showCalendar)}
+              icon={faCalendarDays}
+              className={search.icon}
+            />
+            <span
+              onClick={() => setShowCalendar(!showCalendar)}
+              className={search.text}
+            >
+              {`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
+                date[0].endDate,
+                "dd/MM/yyyy"
+              )}`}
+            </span>
+            {showCalendar && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className={search.date_selector}
+              />
+            )}
+          </li>
+          <li className={search.item}>
+            <FontAwesomeIcon
+              onClick={() => setShowOptions(!showOptions)}
+              icon={faContactBook}
+              className={search.icon}
+            />
+            <span
+              onClick={() => setShowOptions(!showOptions)}
+              className={search.text}
+            >
+              {`${options.adult} adult . ${options.children} children . ${options.room} room`}
+            </span>
+            {showOptions && (
+              <ul className={search.options__container}>
+                <li className={search.option}>
+                  <span className={search.option_label}>adult</span>
+                  <div className="">
+                    <button
+                      disabled={options.adult <= 1}
+                      onClick={() => handleOptionBtn("adult", "decrement")}
+                      className={search.option_counter_btn}
+                    >
+                      -
+                    </button>
+                    <span className={search.option_counter_val}>
+                      {options["adult"]}
+                    </span>
+                    <button
+                      disabled={options.adult === 10}
+                      onClick={() => handleOptionBtn("adult", "increment")}
+                      className={search.option_counter_btn}
+                    >
+                      +
+                    </button>
+                  </div>
+                </li>
+                <li className={search.option}>
+                  <span className={search.option_label}>children</span>
+                  <div className="">
+                    <button
+                      disabled={options.children <= 0}
+                      onClick={() => handleOptionBtn("children", "decrement")}
+                      className={search.option_counter_btn}
+                    >
+                      -
+                    </button>
+                    <span className={search.option_counter_val}>
+                      {options["children"]}
+                    </span>
+                    <button
+                      disabled={options.children === 10}
+                      onClick={() => handleOptionBtn("children", "increment")}
+                      className={search.option_counter_btn}
+                    >
+                      +
+                    </button>
+                  </div>
+                </li>
+                <li className={search.option}>
+                  <span className={search.option_label}>room</span>
+                  <div className="">
+                    <button
+                      disabled={options.room <= 1}
+                      onClick={() => handleOptionBtn("room", "decrement")}
+                      className={search.option_counter_btn}
+                    >
+                      -
+                    </button>
+                    <span className={search.option_counter_val}>
+                      {options["room"]}
+                    </span>
+                    <button
+                      disabled={options.room === 10}
+                      onClick={() => handleOptionBtn("room", "increment")}
+                      className={search.option_counter_btn}
+                    >
+                      +
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li className={search.item}>
+            <button className={search.btn}>search</button>
+          </li>
+        </ul>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
